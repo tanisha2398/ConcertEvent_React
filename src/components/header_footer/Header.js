b02a1b7c8aec7a95cfd2fcb78f39c339200400a3
tanisha2398 <tanishanegipro@gmail.com>
2019-12-20 23:56:56 +0530

    add side drawer with list item

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import MenuIcon from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
+import SideDrawer from "./sideDrawer";
 class Header extends Component {
+  state = {
+    drawerOpen: false
+  };
+
+  toggleDrawer = value => {
+    this.setState({
+      drawerOpen: value
+    });
+  };
+
   render() {
     return (
       <AppBar
@@ -21,6 +32,18 @@ class Header extends Component {
             </div>
             <div className="header_logo_title">Musical Events</div>
           </div>
+
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          />
         </Toolbar>
       </AppBar>
     );
